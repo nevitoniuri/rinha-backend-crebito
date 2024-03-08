1bc05d28f13e39fa4c2499e4c36f53fbee5332d3
Iuri Néviton <iurineviton@gmail.com>
2024-03-08 18:40:23 -0300

    feature: add enum method

--- src/main/java/com/nevitoniuri/rinhabackend/model/enums/TipoTransacao.java
+++ src/main/java/com/nevitoniuri/rinhabackend/model/enums/TipoTransacao.java
@@ -1,6 +1,17 @@
 package com.nevitoniuri.rinhabackend.model.enums;
 
+import com.nevitoniuri.rinhabackend.exception.ServiceError;
+import com.nevitoniuri.rinhabackend.exception.ServiceException;
+
 public enum TipoTransacao {
   DEPOSITO,
-  SAQUE
+  SAQUE;
+
+  public static TipoTransacao fromString(String tipo) {
+    return switch (tipo) {
+      case "c" -> DEPOSITO;
+      case "d" -> SAQUE;
+      default -> throw new ServiceException(ServiceError.TIPO_TRANSACAO_INVALIDO);
+    };
+  }
 }
