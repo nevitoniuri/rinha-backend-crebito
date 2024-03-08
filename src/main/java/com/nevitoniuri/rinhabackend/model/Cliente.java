311bbaf17ce09f242a14bb5b556b3b5f5201b96b
Iuri Néviton <iurineviton@gmail.com>
2024-03-08 18:39:56 -0300

    feature: add Cliente nome

--- src/main/java/com/nevitoniuri/rinhabackend/model/Cliente.java
+++ src/main/java/com/nevitoniuri/rinhabackend/model/Cliente.java
@@ -21,6 +21,7 @@ public class Cliente {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  private String nome;
   private Integer limite;
   private Integer saldo;
 
